f72d465872ee28af9b5739af4bc9730e24059857
Darijan Sekerov <darijan2002@gmail.com>
2021-04-04 14:39:09 +0200

    domasni od cas 3

---
+++ Domasni/Cas03/StudentGroup/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace StudentGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentsG1, studentsG2;
+            studentsG1 = new string[] { "Nick", "Jerald", "Helena", "Kathy", "Logan"};
+            studentsG2 = new string[] { "Jere", "Devon", "Tania", "Norman", "Monroe" };
+
+            Console.WriteLine("Enter student group (1,2): ");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch(choice)
+            {
+                case 1:
+                    Console.WriteLine("Students in G1: ");
+                    for (int i = 0; i < 5; i++) Console.WriteLine(studentsG1[i]);
+                    break;
+                case 2:
+                    Console.WriteLine("Students in G2: ");
+                    for (int i = 0; i < 5; i++) Console.WriteLine(studentsG2[i]);
+                    break;
+                default:
+                    Console.WriteLine("Entered incorrect number!");
+                    break;
+            }
+        }
+    }
+}
---
+++ Domasni/Cas03/SumOfEven/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SumOfEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[6];
+            int sum = 0;
+            for(int i = 0; i < 6; i++)
+            {
+                Console.WriteLine($"Enter integer #{i + 1}: ");
+                arr[i] = int.Parse(Console.ReadLine());
+                sum += arr[i];
+            }
+
+            Console.WriteLine($"The sum is {sum}. ");
+        }
+    }
+}
